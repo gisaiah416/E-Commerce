67ff3916df71d6b6931e96a50c15d71372659291
Isaiah Guardado <gisaiah416@gmail.com>
2024-10-01 22:32:25 -0700

    Added password to requirement

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -2,11 +2,11 @@ const client = require('../config/db.js');
 
 // Create a new user
 exports.createUser = async (req, res) => {
-    const { username, email } = req.body;
+    const { username, email, password } = req.body;
     try {
         const result = await client.query
             (
-                'INSERT INTO users (username, email) VALUES ($1, $2) RETURNING *', [username, email]
+                'INSERT INTO users (username, email, user_pw) VALUES ($1, $2, $3) RETURNING *', [username, email, password]
             );
         res.status(201).json(result.rows[0]);
     } catch (err) {
