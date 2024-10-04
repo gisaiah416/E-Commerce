0118824a79abb1fe95af81c0aa610d670a633100
Isaiah Guardado <gisaiah416@gmail.com>
2024-10-03 22:49:09 -0700

    Checked if email exists before inserting

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -4,18 +4,26 @@ const client = require('../config/db.js');
 exports.createUser = async (req, res) => {
     const { username, email, password } = req.body;
     try {
+        const InvalidEmail = await client.query
+            (
+                'SELECT * FROM USERS WHERE username = $1', [email]
+            );
+
+        if (InvalidEmail.rows.length > 0) {
+            return res.status(409).json({ error: 'Email already exists' });
+        }
+
         const result = await client.query
             (
                 'INSERT INTO users (username, email, user_pw) VALUES ($1, $2, $3) RETURNING *', [username, email, password]
             );
         res.status(201).json(result.rows[0]);
     } catch (err) {
-        if (err.code === '23505') {
-            res.status(409).json({ error: 'Duplicate entry detected' });
-        }
-        else {
-            res.status(500).json({ error: 'Error executing query' });
-        }
+        // if (err.code === '23505') {
+        //     res.status(409).json({ error: 'Duplicate entry detected' });
+        // }
+        res.status(500).json({ error: 'Error executing query' });
+
     }
 };
 
