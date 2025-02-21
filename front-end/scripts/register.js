caebccaafb892d0fae1d0b03650a541536204479
Isaiah Guardado <gisaiah416@gmail.com>
2025-02-21 00:30:46 -0800

    Register Page 1.1 Update

--- front-end/scripts/register.js
+++ front-end/scripts/register.js
@@ -2,5 +2,35 @@ document.getElementById('registerForm')
     .addEventListener('submit', async (e) => {
         e.preventDefault();
 
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const username = document.getElementById('username').value;
+
+        try {
+
+            const response = await fetch('http://localhost:3000/api/users/', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, email, password }),
+            });
+
+            const data = await response.json();
+
+            if (response.status === 200) {
+                console.log('SUCCESS');
+            }
+            else {
+                console.log('ERROR');
+                console.log(data.error)
+            }
+
+        } catch (error) {
+            console.log('Error while creating User', error);
+        }
+
+
+
 
     })
